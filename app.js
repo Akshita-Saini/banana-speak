76245ae3ad11c0c3eab01bc2c3082f7b35aa4510
Akshita-Saini <60578602+Akshita-Saini@users.noreply.github.com>
2020-12-16 01:44:23 +0530

    completed with styling

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("Hello");
+
+/* console.log("Hello");
+var userName = prompt("Enter Name");
+alert("welcome " + userName); */
+
+//var btnTranslate = document.querySelector("#click");
+/*btnTranslate.addEventListener("click", function clickHandler(){
+    console.log("clicked!");
+})
+ */
+
+//document.addEventListener( event , callbackFunc );
+//event can be anything such as 1.hover 2.scroll 3.on-change 4.click 
+//these are pre-defined 
+//when the event occurs, callback will be called
+//callback func is generally that which is called from within another function!
+
+var btnTranslate = document.querySelector("#click");
+var textInput = document.querySelector("#inputDiv");
+var textOutput = document.querySelector("#outputDiv");
+
+function createURL(text)
+{
+    var url = "https://api.funtranslations.com/translate/minion.json?text="+text;
+    return url;
+}
+function clickHandler()
+{
+    var input = textInput.value;
+    //console.log(input);
+    fetch(createURL(input))
+    .then(response => response.json()).then(JSON => textOutput.innerText=JSON.contents.translated).catch(error => console.log(error));
+}
+
+//will give error if clickHandler() is written instead of clickHandler
+btnTranslate.addEventListener("click", clickHandler);
+
